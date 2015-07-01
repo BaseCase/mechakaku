831977e75e29c4d72fc2802e2dc7c60eeb104fde
Casey Brant <casey@caseybrant.com>
2015-07-01 10:44:06 -0700

    getting started on actual class for irc client

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Probably the greatest piece of software ever made.
+
+
+# TODO
+- [ ] learn about the event types so you can write methods for them
--- bot.py
+++ bot.py
@@ -1,11 +1,35 @@
-from irc.client import Reactor
+import os
+
+from irc.client import SimpleIRCClient
+
+
+class KakuClient(SimpleIRCClient):
+    URL = 'irc.twitch.tv'
+    PORT = 6667
+    USERNAME = 'MechaKaku'
+    PASSWORD = os.getenv('MECHAKAKU_PASSWORD')
+    CHANNEL = '#kakusho'
+
+    def __init__(self):
+        if not self.PASSWORD:
+            raise Exception("MECHAKAKU_PASSWORD environment variable not set.")
+        super().__init__()
+
+    def start(self):
+        self.connect(self.URL,
+                     self.PORT,
+                     self.USERNAME,
+                     password=self.PASSWORD)
+        self.connection.join(self.CHANNEL)
+        #super().start()
+
+    def post_message(self, text):
+        self.connection.send_raw("PRIVMSG {0} :{1}".format(self.CHANNEL, text))
 
 
 if __name__ == '__main__':
     print('bot time go!')
 
-    cl = Reactor()
-    server = cl.server()
-    server.connect('irc.twitch.tv', 6667, 'MechaKaku', password='do not commit the secret!')
-    server.join('#kakusho')
-    server.send_raw('PRIVMSG #kakusho :BOOP BEEP')
+    cl = KakuClient()
+    cl.start()
+    cl.post_message('BOOP BEEP')
