b33f5997c18c73eddf5a9af802a0cc29b3172207
Casey Brant <casey@caseybrant.com>
2015-07-01 16:42:44 -0700

    extract config variables into module

--- bot.py
+++ bot.py
@@ -1,38 +1,32 @@
-import os
-
 from irc.client import SimpleIRCClient
+
+import config
 
 
 class KakuClient(SimpleIRCClient):
-    URL = 'irc.twitch.tv'
-    PORT = 6667
-    USERNAME = 'MechaKaku'
-    PASSWORD = os.getenv('MECHAKAKU_PASSWORD')
-    CHANNEL = '#kakusho'
-
     def __init__(self):
-        if not self.PASSWORD:
+        if not config.PASSWORD:
             raise Exception("MECHAKAKU_PASSWORD environment variable not set.")
         super().__init__()
 
     def start(self):
-        self.connect(self.URL,
-                     self.PORT,
-                     self.USERNAME,
-                     password=self.PASSWORD)
-        self.connection.join(self.CHANNEL)
+        self.connect(config.URL,
+                     config.PORT,
+                     config.USERNAME,
+                     password=config.PASSWORD)
+        self.connection.join(config.CHANNEL)
         cl.post_message('/me boots up')
         super().start()
 
     def post_message(self, text):
-        self.connection.send_raw("PRIVMSG {0} :{1}".format(self.CHANNEL, text))
+        self.connection.send_raw("PRIVMSG {0} :{1}".format(config.CHANNEL, text))
 
     def on_pubmsg(self, connection, event):
         user = event.source.split('!')[0]
         text = event.arguments[0]
 
         # figure out what to do based on text (this belongs in a new class probably)
-        if self.USERNAME.lower() in text.lower():
+        if config.USERNAME.lower() in text.lower():
             self.post_message("~TUTURUUUU~")
 
     def on_ctcp(self, connection, event):
---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+URL = 'irc.twitch.tv'
+PORT = 6667
+USERNAME = 'MechaKaku'
+PASSWORD = os.getenv('MECHAKAKU_PASSWORD')
+CHANNEL = '#kakusho'
